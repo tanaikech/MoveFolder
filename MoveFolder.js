8cd3b62fe94a2bd82a7f41e41ef5dc1843812340
tanaikech <tanaike@hotmail.com>
2024-06-06 11:40:07 +0900

    1st commit

---
+++ MoveFolder.js
@@ -0,0 +1,270 @@
+/**
+ * GitHub  https://github.com/tanaikech/MoveFolder<br>
+ * Library name
+ * @type {string}
+ * @const {string}
+ * @readonly
+ */
+var appName = "MoveFolder";
+
+
+/**
+ * ### Description
+ * Check whether Drive API is enabled at Advanced Google services, and return it as true or false and the version.
+ * ref: https://medium.com/google-cloud/checking-api-enabled-with-advanced-google-services-using-google-apps-script-572bcdeb39a8
+ *
+ * @param {String} apiName API name you want to check.
+ * @returns {Object} Object including "api" and "version" properties.
+ */
+function isAPIAtAdvancedGoogleServices_(apiName) {
+  if (!apiName || apiName == "" || typeof apiName != "string") {
+    throw new Error("Please set a valid API name.");
+  } else if (!/^[A-Z]+$/g.test(apiName[0])) {
+    const [t, ...b] = apiName;
+    apiName = [t.toUpperCase(), ...b].join("");
+  }
+  const obj = { apiName, api: "disable" };
+  if (typeof this[apiName] !== "undefined") {
+    obj.api = "enable";
+    obj.version = this[apiName].getVersion();
+  }
+  return obj;
+}
+
+/**
+ * ### Description
+ * Retrieve file list including subfolders.
+ * 
+ * @param {Object} object Object for using this method.
+ * @param {String} object.headers Request header.
+ * @param {String} object.srcFolderId Source folder ID.
+ * @returns {Object} Object including the retrieved file list.
+ */
+function listFolders_(object) {
+  const { headers, srcFolderId } = object;
+
+  // ref: https://github.com/tanaikech/UtlApp?tab=readme-ov-file#addqueryparameters
+  function addQueryParameters(url, obj) {
+    if (url === null || obj === null || typeof url != "string") {
+      throw new Error("Please give URL (String) and query parameter (JSON object).");
+    }
+    return (url == "" ? "" : `${url}?`) + Object.entries(obj).flatMap(([k, v]) => Array.isArray(v) ? v.map(e => `${k}=${encodeURIComponent(e)}`) : `${k}=${encodeURIComponent(v)}`).join("&");
+  }
+
+  const url = "https://www.googleapis.com/drive/v3/files";
+
+  const getAllFolders = (id, parents = [], folders = { temp: [] }) => {
+    const query = {
+      q: `'${id}' in parents and mimeType='application/vnd.google-apps.folder' and trashed=false`,
+      fields: "files(id,name,parents),nextPageToken",
+      pageSize: 1000,
+      supportsAllDrives: true,
+      includeItemsFromAllDrives: true,
+    };
+    const files = [];
+    let pageToken = "";
+    do {
+      const res = UrlFetchApp.fetch(addQueryParameters(url, query), { headers });
+      const obj = JSON.parse(res.getContentText());
+      if (obj.files.length > 0) {
+        files.push(...obj.files);
+      }
+      pageToken = obj.nextPageToken;
+      query.pageToken = pageToken;
+    } while (pageToken)
+    const temp = [];
+    const p = parents.slice();
+    p.push(id);
+    files.forEach(e => {
+      temp.push({ "name": e.name, "id": e.id, "parent": e.parents[0], "tree": p });
+    });
+    if (temp.length > 0) {
+      folders.temp.push(temp);
+      temp.forEach(e => {
+        getAllFolders(e.id, e.tree, folders);
+      });
+    }
+    return folders;
+  }
+
+  // Retrieve folder tree.
+  const res = UrlFetchApp.fetch(`https://www.googleapis.com/drive/v3/files/${srcFolderId}?supportsAllDrives=true&fields=id%2Cname`, { headers });
+  const topFolder = JSON.parse(res.getContentText());
+  const obj = getAllFolders(srcFolderId);
+  const { id, id2Name } = obj.temp.reduce((o, e) => {
+    e.forEach(({ name, id, tree }) => {
+      o.id.push([...tree, id]);
+      o.id2Name = { ...o.id2Name, [id]: name };
+    });
+    return o;
+  }, { id: [[topFolder.id]], id2Name: { [topFolder.id]: topFolder.name } });
+  const name = id.map(e => e.map(f => id2Name[f]));
+  const folderTree = { id, name };
+
+  // Retrieve files from each folder.
+  const files = folderTree.id.map((r, i) => {
+    const id = r[r.length - 1];
+    const query = {
+      q: `'${id}' in parents and trashed=false`,
+      fields: "files(parents,id),nextPageToken",
+      pageSize: 1000,
+      supportsAllDrives: true,
+      includeItemsFromAllDrives: true,
+    };
+    const files = [];
+    let pageToken = "";
+    do {
+      const res = UrlFetchApp.fetch(addQueryParameters(url, query), { headers });
+      const obj = JSON.parse(res.getContentText());
+      if (obj.files.length > 0) {
+        files.push(...obj.files);
+      }
+      pageToken = obj.nextPageToken;
+      query.pageToken = pageToken;
+    } while (pageToken);
+    return {
+      folderTreeById: r,
+      folderTreeByName: folderTree.name[i],
+      filesInFolder: files
+    };
+  });
+  return { files };
+}
+
+/**
+ * ### Description
+ * Move folder including files and folders from the source folder to the destination folder.
+ * 
+ * @param {Object} object Object for using this method.
+ * @param {String} object.srcFolderId Source folder ID.
+ * @param {String} object.dstFolderId Destination folder ID.
+ * @param {String} object.accessToken Access token.
+ * @param {Boolean} object.forSharedDrive Default is false.
+ * @returns {void}
+ */
+function moveFolder_(object) {
+  let { srcFolderId, dstFolderId, accessToken = ScriptApp.getOAuthToken(), forSharedDrive = false } = object;
+
+  const headers = { authorization: "Bearer " + accessToken };
+
+  // Check source folder.
+  console.log("Check source folder.");
+  const checkSharedDrive_ = folderId => {
+    const res = UrlFetchApp.fetch(`https://www.googleapis.com/drive/v3/files/${folderId}?supportsAllDrives=true&fields=driveId`, { headers });
+    const obj = JSON.parse(res.getContentText());
+    return "driveId" in obj;
+  }
+  if (forSharedDrive === false && ![srcFolderId, dstFolderId].some(id => checkSharedDrive_(id))) {
+    console.log("Move in no shared drive.");
+    UrlFetchApp.fetch(`https://www.googleapis.com/drive/v3/files/${srcFolderId}?supportsAllDrives=true&enforceSingleParent=true&addParents=${dstFolderId}`, { method: "PATCH", headers });
+    console.log("Done.");
+    return;
+  }
+
+  // Retrieve file list with the subfolders.
+  console.log("Move in the shared drive.");
+  console.log(`Retrieve file list with the subfolders.`);
+  const obj = listFolders_({ headers, srcFolderId });
+  if (obj.files.length == 0) {
+    console.warn("No files.");
+    return;
+  }
+
+  // Create folder tree to the destination folder.
+  console.log(`Create folder tree to the destination folder.`);
+  const tree = obj.files.map(({ folderTreeById, folderTreeByName }) => ({ folderTreeById, folderTreeByName }));
+  const newFolders = tree.reduce((o, { folderTreeById, folderTreeByName }) => {
+    folderTreeByName.forEach((name, j) => {
+      const parent = j == 0 ? dstFolderId : o[folderTreeById[j - 1]].id;
+      if (!Object.entries(o).some(([k, v]) => k == folderTreeById[j] && v.name == name)) {
+        const options = {
+          headers,
+          contentType: "application/json",
+          payload: JSON.stringify({ name, parents: [parent], mimeType: MimeType.FOLDER })
+        };
+        const res = UrlFetchApp.fetch("https://www.googleapis.com/drive/v3/files?supportsAllDrives=true", options);
+        const { id } = JSON.parse(res.getContentText());
+        o[folderTreeById[j]] = { id, name };
+      }
+    });
+    return o;
+  }, {});
+
+  // Create request body for moving files.
+  console.log(`Create request body for moving files.`);
+  const allFolders = [...new Set(obj.files.flatMap(({ folderTreeById }) => folderTreeById))];
+  const files = obj.files.map(({ folderTreeById, filesInFolder }) => ({ srcFolder: folderTreeById.pop(), srcFiles: filesInFolder.filter(({ id }) => !allFolders.includes(id)).map(({ id }) => id) }));
+  const { requests, err } = files.reduce((o, { srcFolder, srcFiles }) => {
+    if (newFolders[srcFolder]) {
+      if (srcFiles.length > 0) {
+        srcFiles.forEach(fileId => o.requests.push({
+          method: "PATCH",
+          endpoint: `https://www.googleapis.com/drive/v3/files/${fileId}?supportsAllDrives=true&enforceSingleParent=true&addParents=${newFolders[srcFolder].id}`,
+          requestBody: {},
+        }));
+      }
+    } else {
+      o.err.push(`"${srcFolder}" is not included in newFolders.`);
+    }
+    return o;
+  }, { err: [], requests: [] });
+  if (err.length > 0) {
+    console.warn(err);
+  }
+  if (requests.length == 0) {
+    console.warn("Files for moving are not found.");
+    return null;
+  }
+  if (allFolders.length > 0) {
+    const addRequests = allFolders.map(id => ({
+      method: "DELETE",
+      endpoint: `https://www.googleapis.com/drive/v3/files/${id}?supportsAllDrives=true`
+    }));
+    requests.push(...addRequests);
+  }
+
+  // Move folder including files and folders with batch requests.
+  console.log(`Move folder including files and folders with batch requests.`);
+  BatchRequest.EDo({ batchPath: "batch/drive/v3", requests, accessToken });
+
+  console.log("Done.");
+}
+
+/**
+ * ### Description
+ * This is the main method. Move folder including files and folders from the source folder to the destination folder.
+ * 
+ * ### Sample script
+ *
+ * ```javascript
+ * const object = {srcFolderId: "###", dstFolderId: "###", accessToken: "###"};
+ * MoveFolder.run(object);
+ * ```
+ * 
+ * - srcFolderId: Required
+ * - dstFolderId: Required
+ * - accessToken: Default is ScriptApp.getOAuthToken(). If you want to use the access token from the service account, please use this.
+ * 
+ * @param {Object} object Object for using this method.
+ * @param {String} object.srcFolderId Source folder ID.
+ * @param {String} object.dstFolderId Destination folder ID.
+ * @param {String} object.accessToken Access token.
+ * @param {Boolean} object.forSharedDrive Default is false.
+ * @returns {void}
+ */
+function run(object) {
+  if (typeof object != "object" || !["srcFolderId", "dstFolderId"].every(e => e in object)) {
+    throw new Error("Please give valid object.");
+  }
+
+  // Check Drive API.
+  console.log("Check Drive API.");
+  if (isAPIAtAdvancedGoogleServices_("Drive").api != "enable") {
+    throw new Error("Please enable Drive API v3 at Advanced Google services. ref: https://developers.google.com/apps-script/guides/services/advanced#enable_advanced_services");
+  }
+
+  moveFolder_(object);
+}
+
+// For directly using this script.
+const MoveFolder = { run };
---
+++ README.md
@@ -0,0 +1,160 @@
+# MoveFolder
+
+<a name="top"></a>
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENCE)
+
+<a name="overview"></a>
+
+![](images/fig1.png)
+
+# Overview
+
+This is a Google Apps Script library for moving a folder including files and folders on Google Drive.
+
+# Description
+
+This library addresses a common challenge: efficiently moving folders, including their subfolders and files, between Google Drives. This encompasses both personal and shared drives using a script. While Google Drive offers straightforward methods for moving individual files between any drives, directly moving entire folders containing subfolders presents limitations, particularly when shared drives are involved. This script bridges that gap by providing a reliable Google Apps Script solution for such scenarios.
+
+Seeing a need, I created this script after receiving several requests. I believe it could be useful for other users, so I published it. If you find this helpful, I'm glad!
+
+# IMPORTANT: Please read this before you test
+
+- **This script automates moving folders and files within your Google Drive, including those in shared drives. Use caution when running the script, as it will modify your file structure.**
+- **For safe testing, create a sample folder and populate it with some files. If you have a large number of folders and files, consider splitting them into smaller groups for individual testing. Unfortunately, limitations in my environment prevent me from comprehensively testing the script with massive amounts of data.**
+- **Please note that we cannot assume any responsibility or liability for any damage or loss caused by this script. Use it at your own risk.**
+
+# Issue and workaround
+
+The report tackles the restriction of directly moving folders into or out of shared drives. When a direct move is attempted, the following error messages appear.
+
+For Drive API
+
+```
+{
+  "error": {
+    "code": 403,
+    "message": "Moving folders into shared drives is not supported.",
+    "errors": [
+      {
+        "message": "Moving folders into shared drives is not supported.",
+        "domain": "global",
+        "reason": "teamDrivesFolderMoveInNotSupported"
+      }
+    ]
+  }
+}
+```
+
+For Drive service (DriveApp)
+
+```
+Exception: Cannot use this operation on a shared drive item.
+```
+
+It seems that this situation is the current specification. [Ref](https://issuetracker.google.com/issues/76201003) and [Ref](https://issuetracker.google.com/issues/207514843) This script in this report overcomes this limitation by employing a multi-step process:
+
+1. **Retrieving Folder Structure**: It meticulously extracts the complete folder hierarchy from the source folder, capturing all subfolders at every level.
+2. **Recreating Folder Structure**: Leveraging the retrieved information, the script meticulously replicates the entire folder structure within the destination drive.
+3. **File Movement**: Individual files are efficiently transferred from the source folder to their corresponding locations within the newly created folder structure in the destination drive.
+4. **Source Folder Removal (Optional)**: Once the files are safely relocated, the script can optionally remove the original source folder to ensure a clean and organized workspace.
+
+By meticulously following these steps, the script effectively migrates entire folders, including subfolders and files, between any Google Drives, regardless of whether they are personal or shared drives. This report delves into the details of this Google Apps Script, providing a step-by-step explanation of its functionality and implementation.
+
+# Usage
+
+## 1. Create a Google Apps Script project
+
+In this report, Google Apps Script is used. Of course, the approach introducing this report can also be used in other languages.
+
+Here, in order to test the following sample scripts, please create a standalone Google Apps Script project. Of course, this script can also be used with the container-bound script.
+
+And, please open the script editor of the Google Apps Script project.
+
+## 2. Install a Google Apps Script library
+
+When I created a script for this, the script was a bit complicated. So, in order to easily use this script, I created it as a Google Apps Script library. Of course, you can see the actual script of this library at [my repository](https://github.com/tanaikech/MoveFolder).
+
+## Library's project key
+
+```
+1UEyIfxDTat6GYRFy5iJ3UGj2QpyVuuQI5i-BsOcHDMr8HadIWailwj4k
+```
+
+<a name="Howtoinstall"></a>
+
+## How to install
+
+In order to use this library, please install this library.
+
+1. [Install BatchRequest library](https://developers.google.com/apps-script/guides/libraries).
+   - Library's project key is **`1UEyIfxDTat6GYRFy5iJ3UGj2QpyVuuQI5i-BsOcHDMr8HadIWailwj4k`**.
+1. For APIs you want to use, please enable the APIs at API console.
+   - Recently, when it enabled APIs, I had an experience that I was made to wait for several minutes for enabling APIs. So when you enabled APIs at API console, if the error related to APIs occurs, please run again after several minutes.
+
+Of course, when you want to directly use the script of this library without installing the library, you can also achieve it. In that case, please copy and paste the script from the repository to your script editor. By this, you can use it.
+
+This library uses the following 2 scopes.
+
+- `https://www.googleapis.com/auth/drive`
+- `https://www.googleapis.com/auth/script.external_request`
+
+Also, this library also uses my library [BatchRequest](https://github.com/tanaikech/BatchRequest).
+
+## 3. Enable Drive API
+
+This script uses Drive API. So, please enable Drive API at Advanced Google services. [Ref](https://developers.google.com/apps-script/guides/services/advanced#enable_advanced_services)
+
+## 4. Sample script
+
+After the library is installed and Drive API is enabled, you can test this script. Please copy and paste the following script to the script editor installing the library.
+
+Please set the source folder ID and the destination folder ID.
+
+```javascript
+function myFunction() {
+  const srcFolderId = "###";
+  const dstFolderId = "###";
+  MoveFolder.run({ srcFolderId, dstFolderId });
+}
+```
+
+When you run the function `myFunction`, the folder of the folder `srcFolderId` is moved to the folder `dstFolderId`.
+
+## 5. Options of this library
+
+This library has the simple options. These can be used in the object of the argument of `run` method.
+
+- `srcFolderId`: Folder ID of the source folder.
+- `dstFolderId`: Folder ID of the destination folder.
+- `accessToken`: Default is `ScriptApp.getOAuthToken()`. For example, when you want to use the service account, you can use the access token from the service account.
+- `forSharedDrive`: Default is false. When this is true, the process mentioned in the "Issue and workaround" section is forcibly run.
+
+# Reference
+
+- [MoveFolder](https://github.com/tanaikech/MoveFolder)
+
+---
+
+<a name="licence"></a>
+
+# Licence
+
+[MIT](LICENCE)
+
+<a name="author"></a>
+
+# Author
+
+[Tanaike](https://tanaikech.github.io/about/)
+
+[Donate](https://tanaikech.github.io/donate/)
+
+<a name="updatehistory"></a>
+
+# Update History
+
+- v1.0.0 (June 6, 2024)
+
+  1. Initial release.
+
+[TOP](#top)
